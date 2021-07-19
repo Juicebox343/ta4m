a8797aaa254ec79ff6a1a171e760c78afea43d98
Joe Heiser <joe@heiserj.com>
2021-07-19 01:06:37 -0700

    Some basic styling in place, variables set for fonts/colors, completed  rough approximation of hero page

--- src/App.js
+++ src/App.js
@@ -3,10 +3,22 @@ import { auth, db } from './firebaseConfig';
 function App() {
   return (
     <div className="App">
-      <header className="intro">
+      
+      <header className="hero">
+        <div className='banner'>
+          <img src="./ta4m-hero.png" alt='four microphones hanging menacingly in homage to TNG four lights scene'/>
+        </div>
+        <div className='titles'>        
+          <h1>There Are Four Mics</h1>
+          <span className='seperator'></span>
+          <span>A Star Trek Podcast</span>
+        </div>
+      </header>
+
+      <div className="intro">
         <h1>Hello there.</h1>
         <img src="./obi.jpg" alt="my boy obi-wan"/>
-      </header>
+      </div>
     </div>
   );
 }
