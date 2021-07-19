c515cd9b77d1d1b0ab09402c31ca501a6f02f7b9
jallend1 <jallend1@gmail.com>
2021-07-19 09:23:19 -0700

    Merges Joe's hero styling into main branch

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
-import AuthContextProvider from "./Contexts/AuthContext";
+import AuthContextProvider from './Contexts/AuthContext';
 import NavBar from './Components/NavBar';
-import Header from "./Components/Header";
+import Header from './Components/Header';
 
 function App() {
   return (
