804b1415510b9e15943f3eec9ee95daa3b45da42
jallend1 <jallend1@gmail.com>
2021-07-20 20:51:32 -0700

    Updates 'for' attributes on NavBar to 'htmlFor'; Adds basic error handling on Podcast page if episodes aren't fetched yet

--- src/Components/Podcast.jsx
+++ src/Components/Podcast.jsx
@@ -1,24 +1,28 @@
 import { Link } from "react-router-dom";
 const Podcast = ({ episode }) => {
-  return (
-    <article className="episode">
-      <header>
-        <Link to={`/posts/${episode.key}`}>
-          <h3>{episode.title}</h3>
-        </Link>
-      </header>
-      <p>
-        Podcast Release Date: <time>{episode.pubDate}</time>
-      </p>
-      <p>Runtime: {episode.length}</p>
-      <p>{episode.briefDescription}</p>
-      <details>
-        <summary>Full Summary</summary>
-        <div dangerouslySetInnerHTML={{ __html: episode.fullDescription }} />
-      </details>
-      <a href={episode.link}>Listen here.</a>
-    </article>
-  );
+  if (episode) {
+    return (
+      <article className="episode">
+        <header>
+          <Link to={`/posts/${episode.key}`}>
+            <h3>{episode.title}</h3>
+          </Link>
+        </header>
+        <p>
+          Podcast Release Date: <time>{episode.pubDate}</time>
+        </p>
+        <p>Runtime: {episode.length}</p>
+        <p>{episode.briefDescription}</p>
+        <details>
+          <summary>Full Summary</summary>
+          <div dangerouslySetInnerHTML={{ __html: episode.fullDescription }} />
+        </details>
+        <a href={episode.link}>Listen here.</a>
+      </article>
+    );
+  } else {
+    return null;
+  }
 };
 
 export default Podcast;
--- src/Components/Sidebar.jsx
+++ src/Components/Sidebar.jsx
@@ -1,19 +1,22 @@
 const Sidebar = () => {
-    return (
-        <header className="sidebar">
-          <img src="./4MicsLogo.png" alt="starfleet command logo with a microphone instead of the insignia" />
-          <p>
-            There Are Four Mics is a weekly podcast dedicated to group
-            discussion of Star Trek.
-          </p>
-          <p>
-            Join your hosts as they make their way through the Star Trek
-            franchise episode by episode, movie by movie, in stardate order.
-            Grab a beer and join us as we talk about the episodes we love and
-            the occasional episode we love to hate.
-          </p>
-        </header>
-    )
-}
+  return (
+    <header className="sidebar">
+      <img
+        src="4MicsLogo.png"
+        alt="starfleet command logo with a microphone instead of the insignia"
+      />
+      <p>
+        There Are Four Mics is a weekly podcast dedicated to group discussion of
+        Star Trek.
+      </p>
+      <p>
+        Join your hosts as they make their way through the Star Trek franchise
+        episode by episode, movie by movie, in stardate order. Grab a beer and
+        join us as we talk about the episodes we love and the occasional episode
+        we love to hate.
+      </p>
+    </header>
+  );
+};
 
 export default Sidebar;
