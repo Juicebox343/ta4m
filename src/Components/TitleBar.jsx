f2066ac5dd1ba03eea14f3708c2d7e1d1a2f15b7
jallend1 <jallend1@gmail.com>
2021-07-22 16:15:59 -0700

    Removes unused authentication code from TitleBar

--- src/Components/TitleBar.jsx
+++ src/Components/TitleBar.jsx
@@ -1,28 +1,4 @@
-// None of this is actually being used but serves as a monument to authentication working
-
-import { useContext } from 'react';
-import { AuthContext } from '../Contexts/AuthContext';
 const TitleBar = () => {
-  // const { user, signInWithGoogle, signOut } = useContext(AuthContext);
-
-  // const isLoggedIn = () => {
-  //   return (
-  //     <div className="nav-login">
-  //       <div>{user.displayName} is now rockin!</div>
-  //       <button onClick={signOut}>Log Out</button>
-  //     </div>
-  //   );
-  // };
-
-  // const isLoggedOut = () => {
-  //   return (
-  //     <div className="nav-login">
-  //       <div>Log in to get rockin!</div>
-  //       <button onClick={signInWithGoogle}>Login</button>
-  //     </div>
-  //   );
-  // };
-
   return ( 
       <header className='header-top'>
         <div>
