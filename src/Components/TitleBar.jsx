586dc26f093cc5b79826c68856163bce09e2d312
jallend1 <jallend1@gmail.com>
2021-07-26 11:21:48 -0700

    Makes site title a link back to home page

--- src/Components/TitleBar.jsx
+++ src/Components/TitleBar.jsx
@@ -1,26 +1,36 @@
+import { NavLink } from 'react-router-dom';
 const TitleBar = () => {
-  return ( 
-      <header className='header-top'>
-        <div>
-          <div className='blue-bar x1'>11</div>
-            <h1>There Are Four Mics</h1>
-            <div className='blue-bar x3'>28</div>
-            <div className='blue-bar x5 hide-me'>61</div>
-            <div className='orange-bar x2'>98</div>
-            <div className='flavor-text-1 x2'>
-              <span>ESTABLISHING SECURE CONNECTION</span>
-              <span>SECURE CONNECTION ENABLED</span>
-              <span>...Welcome, &#123;GUEST &#125; to Starfleet Archive Museum</span>
-              <span>...My name is INDEX, how may I assist you today?</span>
-            </div>
-            <div className='blue-bar x1'>71</div>
-          </div>
-        <div className='narrow-bar'>
-          <div className='blue-bar x8'></div>
-          <div className='orange-bar x2'></div>
-          <div className='blue-bar x4'></div> 
+  return (
+    <header className='header-top'>
+      <div>
+        <div className='blue-bar x1'>11</div>
+        <h1>
+          <NavLink
+            to='/home'
+            style={{ font: 'inherit', textDecoration: 'inherit' }}
+          >
+            There Are Four Mics
+          </NavLink>
+        </h1>
+        <div className='blue-bar x3'>28</div>
+        <div className='blue-bar x5 hide-me'>61</div>
+        <div className='orange-bar x2'>98</div>
+        <div className='flavor-text-1 x2'>
+          <span>ESTABLISHING SECURE CONNECTION</span>
+          <span>SECURE CONNECTION ENABLED</span>
+          <span>
+            ...Welcome, &#123;GUEST &#125; to Starfleet Archive Museum
+          </span>
+          <span>...My name is INDEX, how may I assist you today?</span>
         </div>
-      </header>
+        <div className='blue-bar x1'>71</div>
+      </div>
+      <div className='narrow-bar'>
+        <div className='blue-bar x8'></div>
+        <div className='orange-bar x2'></div>
+        <div className='blue-bar x4'></div>
+      </div>
+    </header>
   );
 };
 
