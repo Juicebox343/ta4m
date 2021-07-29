06b1746fe7132da8ddc96d9975d67b23f4aaaa3e
Joe Heiser <joe@heiserj.com>
2021-07-29 16:06:56 -0700

    adding image url to episode data

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -1,19 +1,21 @@
 const About = () => {
   return (
         <main className='about'>
-          <h2>About</h2>
-          <p>
-            There Are Four Mics is a weekly podcast dedicated to group
-            discussion of Star Trek. Join your hosts as they make their way
-            through the Star Trek franchise episode by episode, movie by movie,
-            in stardate order. Grab a beer and join us as we talk about the
-            episodes we love and the occasional episode we love to hate.
-          </p>
-          <p>Leave us a rating and review on iTunes!</p>
-          <p>
-            Check out our newest show, Disco Night - A Star Trek Discovery
-            Podcast.
-          </p>
+          <div>
+            <h2>About</h2>
+            <p>
+              There Are Four Mics is a weekly podcast dedicated to group
+              discussion of Star Trek. Join your hosts as they make their way
+              through the Star Trek franchise episode by episode, movie by movie,
+              in stardate order. Grab a beer and join us as we talk about the
+              episodes we love and the occasional episode we love to hate.
+            </p>
+            <p>Leave us a rating and review on iTunes!</p>
+            <p>
+              Check out our newest show, Disco Night - A Star Trek Discovery
+              Podcast.
+            </p>
+          </div>
           <div>
             <h3>Active Crew</h3>
             <ul className='crew crew-one'>
--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -8,7 +8,7 @@ const NavBar = () => {
         <div className='orange-bar x1 hide-me'>67</div>
         <li>
 
-          <NavLink to='home' className='page-title' activeClassName='current-page'>Archives</NavLink>
+          <NavLink to='home' className='page-title' activeClassName='current-page'>Home</NavLink>
         </li>
         <div className='orange-bar x1'>72</div>
         <li>
